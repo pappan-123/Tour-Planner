7c9adfbc88c710b46210e58352ccd306d272f059
pappan-123 <85434109+pappan-123@users.noreply.github.com>
2023-03-21 02:01:42 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,17 @@
+import Card from "./Card";
+function Tours({ tours, removeTour }) {
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title">Tour Planner</h2>
+      </div>
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Tours;
